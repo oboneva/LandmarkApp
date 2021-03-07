9a48cbd50e2231eed39a40faed065c0046cc50f7
Ognyanka Boneva <ogi.boneva@gmail.com>
2021-03-07 14:11:36 +0200

    Creating a watchOS App

---
+++ WatchLandmarks Extension/HostingController.swift
@@ -0,0 +1,16 @@
+//
+//  HostingController.swift
+//  WatchLandmarks Extension
+//
+//  Created by Ognyanka Boneva on 7.03.21.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
